8a4c9c6cfa91e9613a6e8049ab7db9ab094d84b1
nou-ali <noura4403@gmail.com>
2021-11-22 23:53:00 +0000

    The tail is meant to be every element except the head (first element) of the array.

---
+++ tail.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+let newArr = [];
+const tail = function(value) {
+  newArr = value;
+  newArr = newArr.slice(1);
+  return newArr;
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+
+
