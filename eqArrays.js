3dace07aeb3ce5745c2fa77ebb37153f6af1e78c
nou-ali <noura4403@gmail.com>
2021-11-23 23:17:43 +0000

    Checking if two arrays match

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// we need to loop through two arrays in order to see if they match
+const eqArrays = function(arr1, arr2) {
+  let result = true; 
+  //if two arrays are not the same length the assertion fails
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
+  
+  // if the two array of the same length
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+    return result;
+  }
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should NOT PASS
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should NOT PASS
+
+
+
+
+
