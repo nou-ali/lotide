7be889d3f375d3721cce6a1d7e7e139e20e09866
nou-ali <noura4403@gmail.com>
2021-11-24 20:22:50 +0000

    Function takes a collection of items and return counts

--- countOnly.js
+++ countOnly.js
@@ -35,23 +35,26 @@ const countOnly = function(allItems, itemsToCount) { //declaring function
   }
 
 
-//const firstNames = [
- //"Karl",
- //"Salima",
- //"Agouhanna",
- //"Fang",
- //"Kavith",
- //"Jason",
- //"Salima",
- // "Fang",
-  //"Joe"
-//];
+const firstNames = [
+ "Karl",
+ "Salima",
+ "Agouhanna",
+ "Fang",
+ "Kavith",
+ "Jason",
+ "Salima",
+ "Fang",
+  "Joe"
+];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true});
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined); 
 assertEqual(result1["Fang"], 2);
+
+
+
 
 
 
