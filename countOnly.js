4390b0dc56fb6af99d657021d1b3d4baf0cfa717
nou-ali <noura4403@gmail.com>
2021-11-24 14:15:34 +0000

    Function takes a collection of items and return counts for a specific subset of those items

--- countOnly.js
+++ countOnly.js
@@ -47,9 +47,11 @@ const firstNames = [
   "Joe"
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true});
 
 assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Karima"], undefined); 
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+
+
+
