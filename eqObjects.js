65d02468dad444d1db0da3d36ac079d6b2bcec7a
nou-ali <noura4403@gmail.com>
2021-11-25 01:42:14 +0000

    Working with object key iterations - runs correctly

--- eqObjects.js
+++ eqObjects.js
@@ -23,7 +23,22 @@ const eqArrays = function(arr1, arr2) {
     }
   };
 
-
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) { // if objects are equal length 
+    for (let key of Object.keys(object1) ) { // looping through object.keys
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { //Array.isArray() method determines whether the passed value is an Array. syntax is Array.isArray(value)
+        if (!eqArrays(object1[key], object2[key])) {  // not equal return false
+          return false;
+        }
+      }
+    }
+    return true
+  }
+  return false
+  
+  };
 
 
 const ab = { a: "1", b: "2" };
