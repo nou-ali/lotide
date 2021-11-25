98a96d0eb3cb87099582afbb5371e0f7442aa272
nou-ali <noura4403@gmail.com>
2021-11-25 00:39:18 +0000

    Work in progress added eqArray, assert and test cases for new object function

---
+++ eqObjects.js
@@ -0,0 +1,52 @@
+// we need to loop through two arrays in order to see if they match
+const eqArrays = function(arr1, arr2) {
+  let result = true; 
+  //if two arrays are not the same length the assertion fails
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
+  
+  // if the two array of the same length
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+    return result;
+  }
+
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+    }
+  };
+
+
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+
+const abc = { a: "1", b: "2", c: "3"};
+console.log(assertEqual(eqObjects(ab, abc), false)); // => false
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(assertEqual(eqObjects(cd, dc), true)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(assertEqual(eqObjects(cd, cd2), false )); // => false
+
+
+
+
+
+
+
+
+
+
