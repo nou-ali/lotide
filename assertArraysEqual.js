1fbd5a877a4c0d40915c903b217d7dba64e5992c
nou-ali <noura4403@gmail.com>
2021-11-29 12:38:59 +0000

    created module.export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,21 @@
+const eqArrays = require('../eqArrays');
 //copied eqArray function:
 // we need to loop through two arrays in order to see if they match
-const eqArrays = function (arr1, arr2) {
-  let result = true;
-  //if two arrays are not the same length the assertion fails
-  if (arr1.length !== arr2.length) {
-    result = false;
-  }
+// const eqArrays = function (arr1, arr2) {
+//   let result = true;
+// if two arrays are not the same length the assertion fails
+//   if (arr1.length !== arr2.length) {
+//     result = false;
+//   }
 
-  // if the two array of the same length
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      result = false;
-    }
-  }
-  return result;
-}
+//   if the two array of the same length
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       result = false;
+//     }
+//   }
+//   return result;
+// }
 
 
 const assertArraysEqual = function (arr1, arr2) {
@@ -25,5 +26,8 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual(["My", "name", "is", "Noura"], ["My", "name", "is", "Noura"]);
-assertArraysEqual(["Howdy", "hi", "bye"], ["hey", "hi", "bye"]);
+// assertArraysEqual(["My", "name", "is", "Noura"], ["My", "name", "is", "Noura"]);
+// assertArraysEqual(["Howdy", "hi", "bye"], ["hey", "hi", "bye"]);
+
+
+module.exports = assertArraysEqual;
