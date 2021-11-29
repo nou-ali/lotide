37b3c7a8457c33a921473d49726387814ec58cb6
nou-ali <noura4403@gmail.com>
2021-11-29 22:20:18 +0000

    added module export

--- countLetters.js
+++ countLetters.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-
-//The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
-//For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
-//As per assoignment not counting spaces
 const countLetters = function (string) {
   let count = {};
-  
-    // looping through the items
   for (let letter of string) {
     if(letter !== " ") {
       if (count[letter]) {
@@ -26,6 +14,8 @@ const countLetters = function (string) {
   return count;
 }
 
-assertEqual(countLetters("lighthouse in the house")["l"], 1);
-assertEqual(countLetters("lighthouse in the house")["h"], 4);
-assertEqual(countLetters("lighthouse is in daaaaaa the house")["a"], 2);
+// assertEqual(countLetters("lighthouse in the house")["l"], 1);
+// assertEqual(countLetters("lighthouse in the house")["h"], 4);
+// assertEqual(countLetters("lighthouse is in daa the house")["a"], 2);
+
+module.exports = countLetters;
