e79c2f4b31a7c6de8c92755cc8dffd4a224b4da5
nou-ali <noura4403@gmail.com>
2021-11-29 14:16:16 +0000

    corrected test case

--- head.js
+++ head.js
@@ -16,4 +16,3 @@ module.exports = head;
 // assertEqual(head([5, 6, 7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 // assertEqual(head([20]), 20);
-// assertEqual(head([]),);
