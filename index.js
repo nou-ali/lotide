29389e41b87594a42647af970e62c78b93d22b4c
nou-ali <noura4403@gmail.com>
2021-11-29 23:17:07 +0000

    refactored code

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countOnly,
+  countLetters,
+  findKeyByValue,
+  eqArrays,
+  eqObjects
+
+};
