5157a792cd06a938fc2b886ca17535522a368f51
nou-ali <noura4403@gmail.com>
2021-11-29 23:08:53 +0000

    refactored code

--- middle.js
+++ middle.js
@@ -1,29 +1,5 @@
 //const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
-//eqArray function
-// const eqArrays = function (arr1, arr2) {
-//   let result = true;
-//   if (arr1.length !== arr2.length) {
-//     result = false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       result = false;
-//     }
-//   }
-//   return result;
-// }
-
-
-// const assertArraysEqual = function (arr1, arr2) {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`✅  Arrays are equal`);
-//   } else {
-//     console.log(`🛑 Arrays are not equal`);
-//   }
-// };
+//const assertArraysEqual = require('./assertArraysEqual');
 
 //We need to return the middle numbers within an array
 const middle = function (array) {
