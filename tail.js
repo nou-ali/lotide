bff510b0ab38e5c5f0226e3e8382491de706ff75
nou-ali <noura4403@gmail.com>
2021-11-29 17:16:23 +0000

    added another test case

--- tail.js
+++ tail.js
@@ -1,20 +1,22 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 let tail = function (array) {
   return array.slice(1);
 };
 
-// Test Cases Check the returned array elements
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2); // ensure we get back two elements
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+//TEST CASES
+// const output = tail(["Hello", "Lighthouse", "Labs"]);
+//assertEqual(output.length, 3); // ensure we get back three elements
+// assertEqual(output.length, 2); // ensure we get back two elements
+// assertEqual(output[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(output[1], "Labs"); // ensure second element is "Labs"
 
 module.exports = tail;
 
