f5696a5685ac5855786e7119632abd146fe7c812
nou-ali <noura4403@gmail.com>
2021-11-29 23:45:24 +0000

    set up project with Mocha and Chai

---
+++ test/findKeyTest.js
@@ -0,0 +1,33 @@
+//const assertEqual = require('../assertEqual');
+const findKey = require('../findKey');
+const assert = require('chai').assert;
+
+
+describe("#findKey", () => {
+
+  const obj = {
+    
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+it("returns noma", () => {
+  assert.deepEqual(findKey(obj, x => x.stars === 2),"noma");
+});
+
+});
+  
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2),"noma");
+
