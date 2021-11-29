20dac79c9b63b39e11a9532ea9fa62e68d88e1cd
nou-ali <noura4403@gmail.com>
2021-11-29 19:50:16 +0000

    added module.exports

--- middle.js
+++ middle.js
@@ -47,13 +47,13 @@ const middle = function (array) {
 
 // TEST CODE
 
-assertArraysEqual(middle([1]), []) // => [] // returns an empty array
-assertArraysEqual(middle([1, 2]), []) // => [] // returns an empty array
+// assertArraysEqual(middle([1]), []) // => [] // returns an empty array
+// assertArraysEqual(middle([1, 2]), []) // => [] // returns an empty array
 
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+// assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]) // => [3, 4]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2,3]) // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]) // => [3, 4]
 
 module.exports = middle;
