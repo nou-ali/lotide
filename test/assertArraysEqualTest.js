37cc3355482d6b46a623d7e73e9705c46f6bc8bd
nou-ali <noura4403@gmail.com>
2021-11-30 00:38:24 +0000

    created test file

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -3,7 +3,7 @@ const assertEqual = require('../assertEqual');
 
 
 
-// TEST CODE
+//TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should NOT PASS
 
